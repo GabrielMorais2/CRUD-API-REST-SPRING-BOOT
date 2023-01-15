714c0c284e2047343c857e366306aed43e7e14a9
GabrielMorais2 <moraisgabriel274@gmail.com>
2023-01-15 15:26:26 -0300

    Adicionando entity Paciente e seus atributos. Realizado nova migration para criação da tabela paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,14 +1,23 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosCadastroPaciente;
-import org.springframework.web.bind.annotation.*;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repositoy;
+
     @PostMapping
     public void Cadastrar(@RequestBody DadosCadastroPaciente dadosPacientes){
-        System.out.println(dadosPacientes);
+        repositoy.save(new Paciente(dadosPacientes));
     }
 }
