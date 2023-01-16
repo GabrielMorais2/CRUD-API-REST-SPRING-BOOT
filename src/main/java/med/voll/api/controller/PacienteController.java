5bb7855bc725c4639ee4e5f2a7eb55a75251ca46
GabrielMorais2 <moraisgabriel274@gmail.com>
2023-01-16 20:10:10 -0300

    Adicionado metodo GEt com record DTO (DadosListagemPaciente), filtrando os dados apenas pelo nome, email e cpf. Adicionado tambem paginação e ordenação na classe PacienteController. Ordenação esta por meio de nome asc e size=10.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,13 +2,16 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pacientes")
@@ -20,6 +23,11 @@ public class PacienteController {
     @PostMapping
     public void Cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosPacientes){
         repository.save(new Paciente(dadosPacientes));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
